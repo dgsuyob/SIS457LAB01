28b459a05e94106307f65737f2e15f00803eb76f
David Suyo <davidsuyob@gmail.com>
2022-09-22 19:39:56 -0400

    añadiendo hola mundo

---
+++ practica_lab_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hola mundo!" << endl;
+    cin.get();
+    return 0;
+}
+/*Yo David me comprometo a no repetir la materia de sis 457 y realizar las practicas de la materia,
+lo juro por la virgencita de guadalupe, por Dios y por la patria a no esforzarme e investigar
+por mi cuenta cada tema que llevemos en el semestre.
+Santo Ingeniero deme su bendición.
+*/
